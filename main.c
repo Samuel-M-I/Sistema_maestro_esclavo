89ca2c8b6822593386944067d8c8762ed811aa44
Samuel Marroquín Isaza <samuelmarroquinisaza@gmail.com>
2026-05-17 20:53:14 -0500

    Primer commit

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Usuario
+ *
+ * Created on May 17, 2026, 7:35 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Modulo_maestro_esclavo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Modulo_maestro_esclavo.X.production.hex
