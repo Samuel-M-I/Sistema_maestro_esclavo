e82c7c024f9374e273b2b3291899ad4e7a328b4f
Samuel Marroquín Isaza <samuelmarroquinisaza@gmail.com>
2026-05-18 19:29:21 -0500

    Se creo el main de prueba para el modulo maestro esclavo

--- main.c
+++ main.c
@@ -12,8 +12,8 @@
 // CONFIG
 #pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config PWRTE = ON      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON      // Brown-out Reset Enable bit (BOR disabled)
 #pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
 #pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
@@ -23,12 +23,21 @@
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
+#include <pic16f877a.h>
 #include "UART.h"
 
 #define _XTAL_FREQ 20000000
 
 void main(void) {
-    USART_Init()
-    
-    return;
+    USART_Init();
+    TRISB=0;
+    while(1){
+        char dato =USART_Read();
+        if(dato == 'a'){
+            RB0=1;
+        }
+        else if(dato == 'b'){
+            RB0=0;
+        }
+    }
 }
